8366854f03d33e8aff8fbbe3fa9ee1c1ee6b6dfd
Sattriya <satriyabumiharjap@gmail.com>
2026-08-18 14:30:46 +0700

    update: fix rady to up

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,8 @@ import { clerkMiddleware, getAuth } from '@clerk/express'
 import fileUpload from 'express-fileupload'
 import path from 'path'
 import cors from 'cors'
+import cron from 'node-cron'
+import fs from 'fs'
 
 import userRoutes from './routes/user.route.js'
 import adminRoutes from './routes/admin.route.js'
@@ -30,6 +32,21 @@ app.use(express.json())
 app.use(clerkMiddleware())
 app.use(fileUpload({ useTempFiles: true, tempFileDir: path.join(__dirname, 'temp'), createParentPath: true, limits: { fileSize: 10 * 1024 * 1024 } }))
 
+const tempDir = path.join(process.cwd(), "tmp")
+cron.schedule('0 * * * *', () => {
+    if (fs.existsSync(tempDir)) {
+        fs.readdir(tempDir, (err, files) => {
+            if (err) {
+                console.log(err)
+                return
+            }
+            for (const file of files) {
+                fs.unlink(path.join(tempDir, file), (err) => { })
+            }
+        })
+    }
+})
+
 app.use("/api/users", userRoutes)
 app.use("/api/admin", adminRoutes)
 app.use("/api/auth", authRoutes)
@@ -37,6 +54,12 @@ app.use("/api/songs", songsRoutes)
 app.use("/api/albums", albumsRoutes)
 app.use("/api/stats", statsRoutes)
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
 
 app.use((err, req, res, next) => {
     res.status(500).json({ message: process.env.NODE_ENV === "production" ? "Internal server error" : err.message })
