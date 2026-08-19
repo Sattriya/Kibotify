59fa4b7184ea4cf01bdc79ce30b0afe6d069e81f
Sattriya <satriyabumiharjap@gmail.com>
2026-08-19 13:32:29 +0700

    ready test

--- backend/src/index.js
+++ backend/src/index.js
@@ -55,12 +55,12 @@ app.use("/api/songs", songsRoutes)
 app.use("/api/albums", albumsRoutes)
 app.use("/api/stats", statsRoutes)
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")))
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"))
-    })
-}
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname, "../frontend/dist")))
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"))
+//     })
+// }
 
 app.use((err, req, res, next) => {
     res.status(500).json({ message: process.env.NODE_ENV === "production" ? "Internal server error" : err.message })
